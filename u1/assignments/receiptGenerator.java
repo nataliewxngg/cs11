f7fef74dd64d0e0eefbc3a3a5476afaf3677119e
natalie <wong.lt.natalie@gmail.com>
2023-02-15 20:51:21 -0500

    DONE RECEIPT GENERATOR

--- u1/assignments/receiptGenerator.java
+++ u1/assignments/receiptGenerator.java
@@ -3,6 +3,7 @@ package u1.assignments;
 import java.util.Scanner;
 
 public class receiptGenerator {
+
     public static String displayHeader(String header) { // displays the ><><>...<><><
         String headerFinal = "";
         for (int i = 0; i < 25; i++) {
@@ -15,41 +16,43 @@ public class receiptGenerator {
     public static void main(String[] args) {
 
         // variables
-        String header = "><"; // display repeatedly 25x
+        String header = "><"; 
         
         int numOfApples = 0;
         int numOfOranges = 0;
-        double numOfLychees = 0;
-        double numOfBlueberries = 0;
+        double amountOfLychees = 0;
+        double amountOfBlueberries = 0;
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // initiating the scanner for user input
+
+        double subtotal;
+        double netTotal;
 
         // main program/code
 
-        // displays the header + title of receipt
+        // displays the HEADER/TITLE
         displayHeader(header);
         System.out.printf("%36s%n", "Chow Chow Fruit Centre");
         displayHeader(header);
 
-        System.out.println();
-
         // the initial prompt/inputs
-        System.out.printf("%-35s", "Number of apples purchased:");
+        System.out.printf("\n%-35s", "Number of apples purchased:");
         numOfApples = in.nextInt();
 
         System.out.printf("%-35s", "Number of oranges purchased:");
         numOfOranges = in.nextInt();
 
         System.out.printf("%-35s", "Amount of lychees purchased:");
-        numOfLychees = in.nextDouble();
+        amountOfLychees = in.nextDouble();
 
         System.out.printf("%-35s", "Amount of blueberries purchased:");
-        numOfBlueberries = in.nextDouble();
+        amountOfBlueberries = in.nextDouble();
 
-        System.out.println();
+        in.close();
 
         // receipt body (description, quantity, price)
-        for (int i = 0; i < 44; i++) {
+        System.out.println();
+        for (int i = 0; i < 44; i++) { // displays the title for ----receipt---- 
             if (i == 22) {
                 System.out.print("Receipt");
             } else {
@@ -57,13 +60,23 @@ public class receiptGenerator {
             }
         }
         
-        // price description quantity
         System.out.printf("\n\n%-32s%-13sPrice%n", "Description", "Quantity");
+        System.out.printf("%-33s%7d%10.2f%n", "Apples @ $0.03/each", numOfApples, 0.83 * numOfApples);
+        System.out.printf("%-33s%7d%10.2f%n", "Oranges @ $0.75/each", numOfOranges, 0.75 * numOfOranges);
+        System.out.printf("%-33s%7.2f%10.2f%n", "Lychees @ $2.49/lbs", amountOfLychees, 2.49 * amountOfLychees);
+        System.out.printf("%-33s%7.2f%10.2f%n", "Blueberries @ $1.42/lbs", amountOfBlueberries, 1.42 * amountOfBlueberries);
 
-        System.out.printf("%-33s%7d%10.2f%n", "Apples @ $0.83/each", numOfApples, 0.83*numOfApples);
-        System.out.printf("%-33s%7d%10.2f%n", "Oranges @ $0.75/each", numOfOranges, 0.75*numOfOranges);
-        System.out.printf("%-33s%7d%10.2f%n", "Lychees @ $2.49/lbs", numOfLychees, 2.49*numOfLychees);
+        // SUBTOTAL and NET TOTAL
+        subtotal = (0.83 * numOfApples) + (0.75 * numOfOranges) + (2.49 * amountOfLychees)
+                + (1.42 * amountOfBlueberries);
+        netTotal = subtotal * 1.13;
 
-        in.close();
+        System.out.printf("\n%40s%10.2f%n", "Subtotal",subtotal);
+        System.out.printf("%40s%10.2f%n", "H.S.T. (13%)",
+                ((0.83 * numOfApples) + (0.75 * numOfOranges) + (2.49 * amountOfLychees) + (1.42 * amountOfBlueberries))
+                        * 0.13);
+        System.out.printf("\n%40s%10.2f%n", "Net Total", netTotal);
+        
+        System.out.println("\n-------THANK YOU FOR SHOPPING WITH US TODAY!------");
     }
 }
