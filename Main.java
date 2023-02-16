97a8d2278992646ea9173ace0c6a4df748adc055
natalie <wong.lt.natalie@gmail.com>
2023-02-16 10:46:25 -0500

    1.4 special chars

--- Main.java
+++ Main.java
@@ -1,45 +1,39 @@
 import java.util.*;
 
 public class Main {
-  //We want to make a row of bricks that is goal inches long. 
-  //We have a number of small bricks (1 inch each) and big bricks (5 inches each). 
-  //Return true if it is possible to make the goal by choosing from the given bricks. 
-  //This is a little harder than it looks and can be done without any loops. 
-  public static boolean makeBricks(int small, int big, int goal) {
-    // int sum = 0;
-    // // small = 1 inch, big - 5 inches
-    // for (int i = 0; i < big; i++) {
-    //   sum = sum + 5;
-    //   for (int y = 0; y < small; y++) {
-    //     sum++;
+    //We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) 
+    //and big bricks (5 inches each). Return true if it is possible to make the goal by choosing from the given bricks.
+    public boolean makeBricks(int small, int big, int goal) {
+        // 1 inch - small
+        // 5 inch - big
+        // goal must equal to sum if possible
+        int sum = 0;
 
-    //     if (sum == goal) {
-    //       break;
-    //     }
-    //   }
-    // }
-    
-    // if (sum == goal) {
-    //   return true;
-    // }
-    // else {
-    //   return false;
-    // }
+        for (int i = 0; i<=big; i++) {
+            sum = 5*i;
+            if (sum == goal) {
+                return true;
+            }
+            else {
+                for (int count = 0; count<=small; count++) {
+                    sum = sum + count;
+                    if (sum == goal) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
-  }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
 
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+        boolean d = in.nextBoolean();
 
-    int a = in.nextInt();
-    int b = in.nextInt();
-    int c = in.nextInt();
-
-    boolean d = in.nextBoolean();
-    boolean e = in.nextBoolean();
-    boolean f = in.nextBoolean();
-
-    makeBricks(a,b,d);
-    in.close();
-  }
+        in.close();
+    }
 }
---
+++ u1/L4_specialCharacters.java
@@ -0,0 +1,40 @@
+package u1;
+
+public class L4_specialCharacters {
+    public static void main(String[] args) {
+
+        // question:    if strings are bounded by double quotation marks ""
+        //              How would I display " in a print command
+
+        // answer:      using an escape key (back slash)
+        //      in this course we will cover four special characters:
+        //      \\ <- displays one backslash symbol
+        //      \" <- displays the " mark
+        //      \n <- skips a line
+        //      \t <- creates a tab
+
+        System.out.println("I want to type \"\""); // I want to type ""
+        System.out.println("I want to type \\"); // I want to type \
+        System.out.println("Mark\nville"); 
+        // Mark
+        // ville
+
+        System.out.println("\\\"\\\"\nnn\n\"\\\"\\"); 
+        // \"\"
+        // nn
+        // "\"\
+
+        // to skip a line using printf,
+        // you can use BOTH %n or \n
+        System.out.printf("Mark%nville"); 
+        // however, %n is only for printf
+        System.out.println("Mark%nville");
+
+        // since % is reserved for printf
+        // How would I display the % symbol
+        // using the printf symbol??
+        // eg. Display %s in the console
+        System.out.printf("%%s"); // %% prints one % in printf
+
+    }
+}
