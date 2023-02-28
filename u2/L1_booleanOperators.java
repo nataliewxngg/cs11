708b14b1c52b9473c8efe1e329c71bee2fc32cc1
natalie <wong.lt.natalie@gmail.com>
2023-02-28 10:44:40 -0500

    u2 l1

---
+++ u2/L1_booleanOperators.java
@@ -0,0 +1,68 @@
+package u2;
+
+public class L1_booleanOperators {
+
+    // Boolean Operators
+    // These are operators that results in a Boolean value
+
+    public static void main(String[] args) {
+
+        // == Equals to 
+        System.out.println(3 == 4); // false
+        // Similar to adding an integer
+        // to a float, if you ever perform
+        // any operations that involve a double
+        // and an integer, the integer gets
+        // converted into a double automatically
+        // 3 + 4.5 = 3.0 + 4.5 = 7.5
+        System.out.println(5 == 5.0); // true
+        // DO NOT USE == ON STRINGS
+
+        System.out.println("abc".equals("abc")); // true
+        // to compare two strings, you MUST
+        // use the .equals() method
+
+        System.out.println('a'=='A'); // false
+        // You can compare chars with the == operator
+
+        // ----------------------------------------------
+
+        // != NOT Equal Operator
+        System.out.println(5 != 5); // false
+        System.out.println(4.5+3.2 != 1.1); // true
+
+        // -----------------------------------------------
+
+        // < Lesser Than Operator
+        System.out.println(4.5 < 9/2); // false (they are both equal)
+        System.out.println(9/2 < 4.5); // false (they are both equal)
+
+        System.out.println(5 < 5); // false
+        
+        // -----------------------------------------------
+
+        // <= Lesser Than OR Equal TO Operator
+        System.out.println(5 <= 5); // true
+
+        // -----------------------------------------------
+
+        // > Greater Than Operator
+        System.out.println('I' > 'A'); // true (increases from A)
+        System.out.println('z' > 'a'); // true
+        System.out.println('9' > '5'); // true
+        System.out.println('h' > 'l'); // false (h is before l)
+        System.out.println('S' > 'T'); // false (S is before T)
+
+        System.out.println('a' > 'T'); // lowerscase alphabets come after uppercase
+
+        // ------------------------------------------------
+
+        // >= Greater OR Equal TO Operator
+        System.out.println();
+
+        // NOTE:
+        // to compare chara cters:
+        // SPACE < Numeric Strings < 'A'-'Z' < 'a'-'z'
+
+    }
+}
---
+++ u2/assignments/numGuessingGame.java
@@ -0,0 +1,53 @@
+package u2.assignments;
+import java.util.Scanner;
+
+// Natalie Wong
+// February 28, 2023
+// numGuessingGame - Users will continuously guess a number between the range of 1-999 (inclusive)
+//                   until the user reaches their limit of 10 tries OR until the user successfully guesses the number.
+
+public class numGuessingGame {
+    public static void main(String[] args) {
+        
+        // vars
+        int secretNum = 59;
+        int guessNum;
+        Scanner in = new Scanner(System.in);
+
+        // main
+        System.out.print("Enter your guess! ");
+        guessNum = in.nextInt();
+
+        for (int chances = 10; chances > 0; chances--) {
+
+            if (guessNum != secretNum) {
+
+                if (chances == 1){
+                    System.out.println("Too bad! Try harder next time!");
+                }
+
+                else if (guessNum > 999 || guessNum < 1) {
+                    System.out.println("Invalid Input! Ensure your number is between 1 and 999!");
+                    chances++;
+                    guessNum = in.nextInt(); 
+                }
+
+                else if (guessNum > secretNum) {
+                    System.out.printf("Choose a smaller number! You have %d guesses left. ", chances-1);
+                    guessNum = in.nextInt(); 
+                }
+
+                else if (guessNum < secretNum) {
+                    System.out.printf("Choose a larger number! You have %d guesses left. ", chances-1);
+                    guessNum = in.nextInt(); 
+                }
+            }
+
+            else {
+                chances = 0;
+                System.out.println("Congratulations! That is the secret number!");
+            }
+
+        }
+    }
+}
