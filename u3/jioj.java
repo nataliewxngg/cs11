27cfc92bf7b5dc50aef4cb4aff38a68e8b263da4
natalie <wong.lt.natalie@gmail.com>
2023-03-31 19:50:47 -0400

    j

--- u3/jioj.java
+++ u3/jioj.java
@@ -8,5 +8,9 @@ public class jioj {
 
     public static void main(String[] args) {
         System.out.println(roundResultDoubleToInt(4.7)); // returns 4
+
+        int a = 34;
+        boolean aRange = a>30 && a<40;
+        System.out.println(aRange);
     }
 }
