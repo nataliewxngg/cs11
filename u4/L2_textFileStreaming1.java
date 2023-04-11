fec299974fe3a4f6bc7bb9b880a6214488824ed5
natalie <wong.lt.natalie@gmail.com>
2023-04-11 10:58:43 -0400

    text file fstermaing

---
+++ u4/L2_textFileStreaming1.java
@@ -0,0 +1,35 @@
+package u4;
+import java.util.Scanner; 
+import java.io.*;
+
+public class L2_textFileStreaming1 {
+    // TEXTFILES are PERMANENT data storage
+
+    // Exceptions such as Arithmetic Exceptions
+    // and String exceptions are known as default exceptions
+
+    // When streaming text files, there is a posibility 
+    // that the file does not exist. However, text file streaming
+    // is NOT a default exception, and hence you need to introduce
+    // that exception into your program
+
+    public static void main(String[] args) throws IOException {
+
+        // To stream a text file, we need to first create a Scanner
+        Scanner inputFile = new Scanner(new File("sample.txt"));
+        // To export a text file, we need to create a PrintWriter
+        PrintWriter outputFile = new PrintWriter(new FileWriter("sample2.txt"));
+
+        String s;
+
+        while (inputFile.hasNextLine()) {
+            s = inputFile.nextLine();
+            System.out.println(s);
+            outputFile.println("**" + s + "***");
+        }
+
+        inputFile.close();
+        outputFile.close();
+
+    }
+}
