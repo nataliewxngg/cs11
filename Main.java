698c70d55728c0e2476b48ea1dfcf55ccf68d94b
natalie <wong.lt.natalie@gmail.com>
2023-04-17 17:44:10 -0400

    fix decrypti

--- Main.java
+++ Main.java
@@ -2,22 +2,31 @@
 import java.io.*;
 
 public class Main {
-  public static String swapEvery2(String s) {
+  public static String charSubDecrypt(String s) {
+    int num;
+    String sub = "aj_e_________________to___rxspvi";
     String newStr = "";
 
-    for (int i = 0; i < s.length(); i+=2) {
-      if (i == s.length()-1) {
-        newStr += s.charAt(i);
+    for (int i = 0; i < s.length(); i++) {
+
+      if (s.charAt(i) == '@' || s.charAt(i) == '=' || s.charAt(i) == '!' || s.charAt(i) == '?'
+          || s.charAt(i) == '*' || s.charAt(i) == '#' || s.charAt(i) == '&' || s.charAt(i) == '$'
+          || s.charAt(i) == '+' || s.charAt(i) == '^' || s.charAt(i) == '%') {
+            num =
+      } 
+      else if (s.charAt(i) == '_') {
+        newStr += " ";
       }
+
       else {
-        newStr += s.charAt(i+1);
         newStr += s.charAt(i);
       }
+
     }
     return newStr;
-  }
+}
 
   public static void main(String[] args) throws IOException {
-    System.out.println(swapEvery2("PINEAPPLE"));
+    System.out.println(charSubDecrypt("PINEAPPLE"));
   }
 }
--- u4/assignments/Cipher.java
+++ u4/assignments/Cipher.java
@@ -1,9 +1,12 @@
 package u4.assignments;
+
 import java.util.Scanner;
 import java.io.*;
 
 public class Cipher {
-    public static String characterSubstitutions(String s) { // for step 2
+
+    // Encryption Methods
+    public static String charSubEncrypt(String s) { // for step 2
         int num;
         String sub = "@___=___!?____*#_&$+_^_%";
         String newStr = "";
@@ -12,7 +15,7 @@ public class Cipher {
 
             if (s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'J'
                     || s.charAt(i) == 'O' || s.charAt(i) == 'P' || s.charAt(i) == 'R' || s.charAt(i) == 'S'
-                    || s.charAt(i) == 'T' || s.charAt(i) == 'V' || s.charAt(i) == 'X' || s.charAt(i) == ' ') {
+                    || s.charAt(i) == 'T' || s.charAt(i) == 'V' || s.charAt(i) == 'X') {
 
                 num = s.charAt(i) - 65;
                 newStr += sub.charAt(num);
@@ -45,13 +48,13 @@ public class Cipher {
         if (s.length()%2==0) { // even length
             String middleLeft = s.substring(s.length()/2-2, s.length()/2);
             String middleRight = s.substring(s.length()/2, s.length()/2+2);
-    
+
             return s.substring(0,s.length()/2-2) + middleRight + middleLeft + s.substring(s.length()/2+2);
         }
         else {
             String middleLeft = s.substring(s.length()/2-1, s.length()/2+1);
             String middleRight = s.substring(s.length()/2+1, s.length()/2+3);
-        
+
             return s.substring(0,s.length()/2-1) + middleRight + middleLeft + s.substring(s.length()/2+3);
         }
     }
@@ -59,35 +62,62 @@ public class Cipher {
     public static String swapEvery2(String s) { // for step 6
         String newStr = "";
 
-        for (int i = 0; i < s.length(); i+=2) {
-          if (i == s.length()-1) {
-            newStr += s.charAt(i);
-          }
-          else {
-            newStr += s.charAt(i+1);
-            newStr += s.charAt(i);
-          }
+        for (int i = 0; i < s.length(); i += 2) {
+            if (i == s.length() - 1) {
+                newStr += s.charAt(i);
+            } else {
+                newStr += s.charAt(i + 1);
+                newStr += s.charAt(i);
+            }
         }
         return newStr;
     }
 
-    public static void Main(String[] args) throws IOException {
+    // Decryption Methods
+    public static String charSubDecrypt(String s) { // for step 5 
+        int num;
+        String sub = "aj_e_________________to___rxspvi";
+        String newStr = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '@' || s.charAt(i) == '=' || s.charAt(i) == '!' || s.charAt(i) == '?'
+                    || s.charAt(i) == '*' || s.charAt(i) == '#' || s.charAt(i) == '&' || s.charAt(i) == '$'
+                    || s.charAt(i) == '+' || s.charAt(i) == '^' || s.charAt(i) == '%') {
+                num = Math.abs(64 - s.charAt(i));
+                newStr += sub.charAt(num);
+            }
+
+            else if (s.charAt(i) == '_') {
+                newStr += " ";
+            }
+
+            else {
+                newStr += s.charAt(i);
+            }
+
+        }
+        return newStr;
+    }
+
+    public static void main(String[] args) throws IOException {
 
         // Global Variables (some initiated by user input)
-        Scanner in = new Scanner(System.in);
+        Scanner inInt = new Scanner(System.in);
+        Scanner inStr = new Scanner(System.in);
 
         System.out.print("Are you encrypting (0) or decrypting (1) an existing file? ");
-        int encryptOrDecrypt = in.nextInt();
-        while (encryptOrDecrypt != 0 || encryptOrDecrypt != 1) {
+        int encryptOrDecrypt = inInt.nextInt();
+        while (encryptOrDecrypt != 0 && encryptOrDecrypt != 1) {
             System.out.print("Invalid input. Are you encrypting (0) or decrypting (1) and existing file? ");
-            encryptOrDecrypt = in.nextInt();
+            encryptOrDecrypt = inInt.nextInt();
         }
 
         System.out.print("What is the name of the input text file? ");
-        String inputTXTName = in.nextLine();
+        String inputTXTName = inStr.nextLine();
 
         System.out.print("What is the intended name for your output text file? ");
-        String outputTXTName = in.nextLine();
+        String outputTXTName = inStr.nextLine();
 
         Scanner inputFile = new Scanner(new File(inputTXTName));
         PrintWriter outputFile = new PrintWriter(new FileWriter(outputTXTName));
@@ -97,42 +127,71 @@ public class Cipher {
         // Main Body Code
 
         // Encryption Process
-        if (encryptOrDecrypt == 0) { 
+        if (encryptOrDecrypt == 0) {
             while (inputFile.hasNextLine()) {
-                
+                s = inputFile.nextLine();
+
                 // step 1 - to uppercase
-                s = (inputFile.nextLine()).toUpperCase(); 
+                s.toUpperCase();
 
                 // step 2 - character substitutions
-                s = characterSubstitutions(s);
-                
+                s = charSubEncrypt(s);
+
                 // step 3 - first half -> last half
                 if ((s.length() % 2) == 0)
                     s = s.substring(s.length() / 2) + s.substring(0, s.length() / 2); // even length of letters
                 else
                     s = s.substring(s.length() / 2 + 1) + s.substring(0, s.length() / 2 + 1); // odd length of letters
-                
+
                 // step 4 - swap 1st 2 chars with last 2
                 s = swapFirstAndLast2Chars(s);
-                
+
                 // step 5 - swap mid two chars
                 s = swapMiddle2(s);
 
                 // step 6 - swap every 2 letters
                 s = swapEvery2(s);
-                
+
+                outputFile.println(s); // Stream to new file
             }
         }
-        
+
         // Decryption Process
-        else { 
-            System.out.println("Decryption not yet completed.");
+        else {
+            while (inputFile.hasNextLine()) {
+                s = inputFile.nextLine();
+
+                // step 1 - swap every 2 letters
+                s = swapEvery2(s);
+
+                // step 2 - swap mid 2 chars
+                s = swapMiddle2(s);
+
+                // step 3 - swap 1st 2 chars with last 2
+                s = swapFirstAndLast2Chars(s);
+
+                // step 4 - first half -> last half
+                if ((s.length() % 2) == 0)
+                    s = s.substring(s.length() / 2) + s.substring(0, s.length() / 2); // even length of letters
+                else
+                    s = s.substring(s.length() / 2 + 1) + s.substring(0, s.length() / 2 + 1); // odd length of letters
+
+                // step 5 - character substitution
+                s = charSubDecrypt(s);
+
+                // step 6 - to lowercase
+                s = s.toLowerCase();
+
+                outputFile.println(s);
+            }
         }
 
         // Termination of Variables
-        in.close();
+        inInt.close();
+        inStr.close();
+
         inputFile.close();
-        outputFile.close();        
+        outputFile.close();
 
     }
 }
