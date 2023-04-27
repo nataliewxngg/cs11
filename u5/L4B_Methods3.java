a7a273a7ea091a348a187a7a7be03b512017099d
natalie <wong.lt.natalie@gmail.com>
2023-04-27 10:27:48 -0400

    qwertyuiopasdfghjklzxcvbnm

--- u5/L4B_Methods3.java
+++ u5/L4B_Methods3.java
@@ -33,24 +33,49 @@ public class L4B_Methods3 {
         return saNew;
     }
 
+    // Write a method called edward, which takes a String array sa 
+    // and an integer array ia. THe method returns a character array
+    // which consists of the character from the String obtained
+    // in sa at the corresponding index obtained in ia
+    // edward({"hello","ics3u","comp sci","class"}, {3,2,100,-2})
+    // -> {'l','s','',''}
+    public static char[] edward(String[] sa, int[] ia) {
+        char[] output = new char[sa.length];
+
+        for (int i = 0; i < sa.length; i++) {
+            if (ia[i] < sa[i].length() && ia[i]>=0) output[i]=sa[i].charAt(ia[i]);
+            else output[i]=' ';
+        }
+        return output;
+    }
+
     public static void main(String[] args) {
-        int[] ia1 = {1,2,4,9};
-        int[] ia2 = increase1(ia1);
-        String[] sa1 = {"a","b","c","d"};
-        String[] sa2 = removeString(sa1,"c");
+        // int[] ia1 = {1,2,4,9};
+        // int[] ia2 = increase1(ia1);
+        // String[] sa1 = {"a","b","c","d"};
+        // String[] sa2 = removeString(sa1,"c");
 
-        for (int i = 0; i < ia1.length; i++) {
-            System.out.println(ia1[i]);
-        }
-        for (int i = 0; i < ia2.length; i++) {
-            System.out.println(ia2[i]);
+        // for (int i = 0; i < ia1.length; i++) {
+        //     System.out.println(ia1[i]);
+        // }
+        // for (int i = 0; i < ia2.length; i++) {
+        //     System.out.println(ia2[i]);
+        // }
+
+        // for (int i = 0; i < sa2.length; i++) {
+        //     System.out.println(sa2[i]);
+        // }
+        // for (int i = 0; i < sa1.length; i++) {
+        //     System.out.println(sa1[i]);
+        // }
+
+        String[] sa3 = {"hello","ics3u","comp sci","class"};
+        int[] ia3 = {3,2,100,-2};
+        char[] ca1 = edward(sa3,ia3);
+
+        for (int i = 0; i < ca1.length; i++) {
+            System.out.println(ca1[i]);
         }
 
-        for (int i = 0; i < sa2.length; i++) {
-            System.out.println(sa2[i]);
-        }
-        for (int i = 0; i < sa1.length; i++) {
-            System.out.println(sa1[i]);
-        }
     }
 }
