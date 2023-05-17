9ea4f4f8f6163b963a2ea950a5ad4c85c937d67c
natalie <wong.lt.natalie@gmail.com>
2023-05-17 12:16:55 -0400

    check

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-
+    int[] ia1 = {1,2,3,4,5};
+    int num = ia1[2];
+    ia1[2]=100;
+    System.out.println(ia1[2]);
+    System.out.println(num);
   }
 }
