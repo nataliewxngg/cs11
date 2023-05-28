3d5c693aff1e5342153b3651e9ab6cedf89fc3ee
natalie <wong.lt.natalie@gmail.com>
2023-05-28 16:45:21 -0400

    new sprites

--- culminating/Main.java
+++ culminating/Main.java
@@ -20,11 +20,16 @@ public class Main extends JPanel implements KeyListener, MouseListener, Runnable
     public static int spriteNo = 0;
     public static int frameController = 0;
 
-    public static int xPos = 320;
-    public static int yPos = 240;
+    public static int dogxPos = 320;
+    public static int dogyPos = 240;
+
+    public static int birdxPos = 0;
+    public static int birdyPos = 180;
 
     public static BufferedImage[] stateImages = new BufferedImage[4];
+
     public static BufferedImage[] dogIdle = new BufferedImage[4];
+    public static BufferedImage[] dogRun = new BufferedImage[6];
 
     public Main() {
         // JPanel default settings
@@ -46,7 +51,7 @@ public class Main extends JPanel implements KeyListener, MouseListener, Runnable
 
         if (state == 0) {
             frameController++;
-            g.drawImage(dogIdle[spriteNo], xPos, yPos, null);
+            g.drawImage(dogIdle[spriteNo], dogxPos, dogyPos, null);
 
             if (frameController == 4) {
                 spriteNo = (spriteNo + 1) % 4;
@@ -71,10 +76,17 @@ public class Main extends JPanel implements KeyListener, MouseListener, Runnable
         try {
             stateImages[0] = ImageIO.read(new File("culminating/assets/menu.png"));
 
-            dogIdle[0] = ImageIO.read(new File("culminating/assets/sprites/dogIdle0.png"));
-            dogIdle[1] = ImageIO.read(new File("culminating/assets/sprites/dogIdle1.png"));
-            dogIdle[2] = ImageIO.read(new File("culminating/assets/sprites/dogIdle2.png"));
-            dogIdle[3] = ImageIO.read(new File("culminating/assets/sprites/dogIdle3.png"));
+            dogIdle[0] = ImageIO.read(new File("culminating/assets/sprites/dog/dogIdle0.png"));
+            dogIdle[1] = ImageIO.read(new File("culminating/assets/sprites/dog/dogIdle1.png"));
+            dogIdle[2] = ImageIO.read(new File("culminating/assets/sprites/dog/dogIdle2.png"));
+            dogIdle[3] = ImageIO.read(new File("culminating/assets/sprites/dog/dogIdle3.png"));
+
+            dogRun[0] = ImageIO.read(new File("culminating/assets/sprites/dog/dogRun0.png"));
+            dogRun[1] = ImageIO.read(new File("culminating/assets/sprites/dog/dogRun1.png"));
+            dogRun[2] = ImageIO.read(new File("culminating/assets/sprites/dog/dogRun2.png"));
+            dogRun[3] = ImageIO.read(new File("culminating/assets/sprites/dog/dogRun3.png"));
+            dogRun[4] = ImageIO.read(new File("culminating/assets/sprites/dog/dogRun4.png"));
+            dogRun[5] = ImageIO.read(new File("culminating/assets/sprites/dog/dogRun5.png"));
 
         } catch (Exception e) {
             System.out.println("Something wrong with the image!");
