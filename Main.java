eb7ebc7565c1af261f977d457cbe42a72b7917df
natalie <wong.lt.natalie@gmail.com>
2023-06-28 09:16:33 -0400

    amanda is so sad

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    String s = "Wong Natalie";
-    System.out.println(s.substring(s.indexOf(" ") + 1) + " " + s.substring(0, s.indexOf(" ")));
-
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.println("*");
+      }
+      System.out.print("*");
+    }
   }
 }
